752068a19a18930f6dac06d0293c2abb0766b00b
leizuoping_lxl@163.com <leizuoping_lxl@163.com>
2017-08-01 09:46:12 +0800

    webpacker

--- module/index.js
+++ module/index.js
@@ -9,3 +9,21 @@ route.init('home');
 	console.log("进读条加载完成");
 	
 })*/
+ var detectBack = {  //监听浏览器 前进后退事件
+        initialize: function() {
+            //监听hashchange事件
+            window.addEventListener('hashchange', function() {
+                //为当前导航页附加一个tag
+                this.history.replaceState('hasHash', '', '');
+            }, false);
+            window.addEventListener('popstate', function(e) {
+                if (e.state) {
+                	alert('我要刷新页面')
+                    //侦测是用户触发的后退操作, dosomething
+                    //这里刷新当前url
+                    this.location.reload(); 
+                }
+            }, false);
+        }
+    }
+    detectBack.initialize();
